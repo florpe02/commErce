b9a33f3f972e3d1059b199c3d002aaba21c0a0b5
florpe02 <maflop2002@gmail.com>
2021-08-22 15:08:56 -0300

    22 08

--- js/login.js
+++ js/login.js
@@ -1,6 +1,11 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+function redireccionar(){
+      
+      
+    window.location.replace('index.html')
+  }
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,31 @@
+const listado = "https://japdevdep.github.io/ecommerce-api/product/all.json"; 
+
+
+    document.getElementById("data").innerHTML = "";
+  
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
 
-});
+    fetch(listado)
+    .then((respuesta) => respuesta.json())
+
+    .then((datos) => {
+        datos.forEach(( products => {
+           
+            document.getElementById("data").innerHTML +=`
+           
+          
+            <h5> `+products.name+` </h5> 
+            <img src="` + products.imgSrc + `" width="300" height="200" alt="` + products.description + `" class="img-thumbnail">    
+           
+             <p> `+ products.description+` </p> 
+            <p> `+ products.cost+products.currency+` </p> 
+              <p> Vendidos : `+products.soldCount+` </p> 
+        
+`;
+        
+    }));
+    })
+    .catch((error) => alert("Hubo un error: " + error));
+;
