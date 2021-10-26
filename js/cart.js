7a8ef338e1faee53c42d692a7e2daf4263e82b75
florpe02 <maflop2002@gmail.com>
2021-10-26 12:39:54 -0300

    26 10

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,22 @@
+document.addEventListener("DOMContentLoaded", function(e){
 
+    getJSONData(CART_INFO_URL).then(function(resultObj){
+      if (resultObj.status === "ok"){
+          cart = resultObj.data;
+          infoCarrito(cart);
+          calculartotal() 
+      }
+    
+  
+       
+  
+      
+  })
+  
+  .catch((error) => alert("Hubo un error: " + error));
+  ;
+  
+  });
 
 var cart={};
 
@@ -22,10 +40,23 @@ function mostrarimagen (array){
             document.getElementById("src").innerHTML = htmlContentToAppend;
         }
     }
+function calculartotal(){
+    let total=0;
+    let subs= document.getElementsByClassName("subtotal");
+    for(let i = 0; i < subs.length; i++){
+        total+= parseInt(subs[i].innerHTML);
+
+    }
+ 
+    document.getElementById("total").innerHTML=total;
+
+
+}
 
 function infoCarrito(cart){
 
   let htmlContentToAppend = "";
+  
 
   for(let i = 0; i < cart.articles.length; i++){
       let article = cart.articles[i];
@@ -33,39 +64,51 @@ function infoCarrito(cart){
       htmlContentToAppend+=
 
       `<hr>
-      <table>
-      <thead>
       <div class="container text-center">
 
       <div class="row border">
-      <div class="col-sm-4 col-md-2 border" style="background-color: white;">
-      <p id="score"> `+article.name+" "+`  </p>
+      <div class="col-sm-4 col-md-4 border" style="background-color: white;">
+      <p id="score"> <br><br><br>`+article.name+" "+`  </p>
   </div>
   
   
 
      
   <div class="col-sm-4 col-md-2 border" style="background-color: white;">
-      <p> Cantidad: `+article.count+ `</p>   
+      <p> <br><br><br>Cantidad: `+article.count+ `</p>   
       </div>
    
 
  
       <div class="col-sm-4 col-md-2 border" style="background-color: white;">
-      <p> Precio: `+ article.unitCost+`  `+article.currency+` </p>
+      <p> <br><br><br>Precio: `+ article.unitCost+`  `+article.currency+` </p>
       </div>
-      </div>
+  
 
 
-      <div class="col-sm-4 col-md-2 border" style="background-color: white;">
+      <div class="col-sm-4 col-md-3 border" style="background-color: white;">
        <img src="` +article.src + `" width="300" height="200"> 
       </div>
+
       </div>
      
        
     </div>
-     
+    <br><br><br>
+    <div class="container text-center">
+   
+    <div class="col-sm-2 col-md-2 border" style="background-color: white;">
+    <p class="subtotal" id="sub${i}> Subtotal: ` +article.unitCost*article.count+ `</p>
+    <p id="total">    </p> Total:
+    <p>Envío: </p>
     
+     
+      <p>Forma de pago: </p>
+      <p> Dirección:  </p>
+    </div>
+    </div>
+
+      </div>
 `
 
       document.getElementById("carrito").innerHTML = htmlContentToAppend;
@@ -73,19 +116,3 @@ function infoCarrito(cart){
 }
 
 
-document.addEventListener("DOMContentLoaded", function(e){
-  getJSONData(CART_INFO_URL).then(function(resultObj){
-    if (resultObj.status === "ok"){
-        cart = resultObj.data;
-        infoCarrito(cart);
-    }
-  
-
-     
-
-    
-})
-.catch((error) => alert("Hubo un error: " + error));
-;
-
-});
